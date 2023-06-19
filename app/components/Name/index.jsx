846d7f6b7117081d3ebda22fe823981cc9b367e0
ptahemdjehuty <pariso03henri@gmail.com>
2023-06-19 13:16:22 +0100

    - add name component

---
+++ app/components/Name/index.jsx
@@ -0,0 +1,16 @@
+import { Box, Center, Heading } from "@chakra-ui/react";
+
+export default function Name() {
+  return (
+    <Center>
+      <Box mt="10">
+        <Heading as="h3" fontSize="3xl">
+          <Box as="span" color="#ff7624">
+            ABIODOUN
+          </Box>{" "}
+          BABA MANDEF
+        </Heading>
+      </Box>
+    </Center>
+  );
+}
