c84cb01e048d83a724d5a8eb7d648366d850af46
ptahemdjehuty <pariso03henri@gmail.com>
2023-06-19 13:17:20 +0100

    - add home bio component

---
+++ app/components/bio/HomeBio/index.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Box, Center, Text } from "@chakra-ui/react";
+
+export default function HomeBio() {
+  return (
+    <Center>
+      <Box>
+        <Text align="justify" fontSize="18" mt="20" mb="5" p="2">
+          Hello! I'm{" "}
+          <Box color="#ff7624" as="span">
+            Abiodoun
+          </Box>
+          , an African full-stack engineer enjoying building apps with python
+          and javascript to tackle everyday challenges. Let's connect and dive
+          into the exciting world of technology and beyond !
+        </Text>
+      </Box>
+    </Center>
+  );
+}
