6e2e62ea185a175a1ffbb90c2c5202f4dd4a834d
ptahemdjehuty <pariso03henri@gmail.com>
2023-06-19 13:17:51 +0100

    - add avatar

---
+++ app/components/Avatar/index.jsx
@@ -0,0 +1,12 @@
+import { Box, Center } from "@chakra-ui/react";
+import Animate from "./Animate";
+
+export default function Avatar() {
+  return (
+    <Center>
+      <Box>
+        <Animate />
+      </Box>
+    </Center>
+  );
+}
