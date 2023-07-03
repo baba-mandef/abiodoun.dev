4129a561b31a38e0d913095b92d863b703c5f59b
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-03 18:13:02 +0100

    - add nav component

--- app/components/partials/Nav.jsx
+++ app/components/partials/Nav.jsx
@@ -7,9 +7,13 @@ import {
   Icon,
   Text,
 } from "@chakra-ui/react";
-import { FaUserCircle, FaFileAlt, FaBriefcase } from "react-icons/fa";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { FaHome, FaUserCircle, FaFileAlt, FaBriefcase } from "react-icons/fa";
 
 function Nav() {
+  const router = useRouter();
   return (
     <>
       <Center>
@@ -19,19 +23,28 @@ function Nav() {
           mx="5"
           pos="fixed"
           bottom="0"
-          pb="60"
+          pb="20px"
           centerContent
         >
           <Box>
             <Divider
               bg="gray"
-              boxShadow="0px 3px 3px rgba(0, 0, 0, 0.5)"
+              //boxShadow="0px 2px 2px rgba(0, 0, 0, 0.3)"
               w="100%"
               h="1px"
             />
 
             <HStack pt="15px">
-              <Box mx="20px">
+            <Box mx="10px" as="button" onClick={() => router.push("/")}>
+                <Center>
+                  <Icon as={FaHome} color={"#ff7624"} boxSize={15} mr="10px" />
+                  <Text fontWeight={"bold"} fontSize={"14"}>
+                    Home
+                  </Text>
+                </Center>
+              </Box>
+
+              <Box mx="10px" as="button" onClick={() => router.push("/about")}>
                 <Center>
                   <Icon
                     as={FaUserCircle}
@@ -39,12 +52,13 @@ function Nav() {
                     boxSize={15}
                     mr="10px"
                   />
-                  <Text fontWeight={"bold"} fontSize={"16"}>
+                  <Text fontWeight={"bold"} fontSize={"14"}>
                     About
                   </Text>
                 </Center>
               </Box>
-              <Box mx="20px">
+
+              <Box mx="10px" as="button" onClick={() => router.push("/posts")}>
                 <Center>
                   <Icon
                     as={FaFileAlt}
@@ -52,12 +66,16 @@ function Nav() {
                     boxSize={15}
                     mr="10px"
                   />
-                  <Text fontWeight={"bold"} fontSize={"16"}>
-                    Blog
+                  <Text fontWeight={"bold"} fontSize={"14"}>
+                    Blog {router.pathname}
                   </Text>
                 </Center>
               </Box>
-              <Box mx="20px">
+              <Box
+                mx="10px"
+                as="button"
+                onClick={() => router.push("/projects")}
+              >
                 <Center>
                   <Icon
                     as={FaBriefcase}
@@ -65,7 +83,7 @@ function Nav() {
                     boxSize={15}
                     mr="10px"
                   />
-                  <Text fontWeight={"bold"} fontSize={"16"}>
+                  <Text fontWeight={"bold"} fontSize={"14"}>
                     Projects
                   </Text>
                 </Center>
