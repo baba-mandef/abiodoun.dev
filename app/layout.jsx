74ae3fcce274a91abc0f9cc07339040d88812aa4
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-03 18:09:16 +0100

    - update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,27 @@
 "use client";
 
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import  Nav  from './components/partials/Nav'
+import { ChakraProvider, extendTheme, Grid, GridItem } from "@chakra-ui/react";
+import Nav from "./components/partials/Nav";
 import theme from "./themes";
-
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>
-          Baba Mandef
-        </title>
-        <meta name="description" content="Imagination, beyond comprehension"></meta>
-        
+        <title>Baba Mandef</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico"></link>
+        <meta
+          name="description"
+          content="Imagination, beyond comprehension"
+        ></meta>
       </head>
-      <body >
+      <body>
         <CacheProvider>
-          <ChakraProvider theme={theme}>{children}</ChakraProvider>
-          
-          <Nav></Nav>
+          <ChakraProvider theme={theme}>
+            {children}
+            <Nav />
+          </ChakraProvider>
         </CacheProvider>
       </body>
     </html>
