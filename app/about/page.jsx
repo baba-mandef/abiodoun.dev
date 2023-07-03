d24d39afa0c9e17700ef7244f3f4bc09687d674f
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-03 18:10:21 +0100

    - add about page

---
+++ app/about/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { Box, Container, Center, Grid, GridItem } from "@chakra-ui/react";
+import Avatar from "../components/Avatar";
+import Name from "../components/Name";
+import AboutDesc from "../components/descpription/AboutDesc";
+import AboutBio from "../components/bio/AboutBio";
+import AboutStack from "../components/AboutStack";
+
+export default function About() {
+  return (
+    <>
+      <Center>
+        <Container maxW="3xl" mb="80px" mt="20">
+          <Grid>
+            <GridItem>
+              <Avatar />
+            </GridItem>
+            <GridItem>
+              <Name />
+            </GridItem>
+            <GridItem>
+              <AboutDesc />
+            </GridItem>
+
+            <GridItem>
+              <AboutBio />
+            </GridItem>
+            <GridItem>
+              <AboutStack />
+            </GridItem>
+          </Grid>
+        </Container>
+      </Center>
+    </>
+  );
+}
