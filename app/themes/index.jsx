a2c83ce3fc61804d7f844499d82b8533414b650e
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-03 18:16:41 +0100

    - add theme

--- app/themes/index.jsx
+++ app/themes/index.jsx
@@ -13,6 +13,13 @@ const theme =  extendTheme({
     heading: quick.style.fontFamily,
     body: quick.style.fontFamily,
   },
+  breakpoints: {
+    sm: '320px',
+    md: '768px',
+    lg: '960px',
+    xl: '1200px',
+    '2xl': '1536px',
+  },
   shadows: {
   
     barShadow: "0px 4px 4px rgba(0, 0, 0, 0.5)",
