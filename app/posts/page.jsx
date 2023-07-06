fa2e602f3a8d6fb5f973d414c9c65f8e346e98b6
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-06 01:02:52 +0100

    - blog

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,16 +1,26 @@
-import Image from 'next/image'
-// import styles from './page.module.css'
-
-
-export default function Posts() {
+"use client";
+import { Box, Center, Container, Grid, GridItem } from "@chakra-ui/react";
+import Avatar from "../components/Avatar";
+import Name from "../components/Name";
+import BlogList from "../components/BlogList";
+export default function Blog() {
   return (
-
+    <>
+      <Center mx="5%" mb="80px" mt="20">
+        
+          <Grid>
+            <GridItem>
+              <Avatar />
+            </GridItem>
+            <GridItem>
+              <Name />
+            </GridItem>
+            <GridItem>
+              <BlogList/>
+            </GridItem>
+          </Grid>
     
-      <>
-          <h1>
-              blog
-          </h1>
-      </>
-
-  )
+      </Center>
+    </>
+  );
 }
