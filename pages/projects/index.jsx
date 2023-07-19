011523ae3d110669b30dc890bd8c539cb628be35
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-19 15:41:10 +0100

    ❇️ init project page

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,9 +1,26 @@
-export default function Posts(){
-    return(
-        <>
-            <h1>
-                projects
-            </h1>
-        </>
-    )
+"use client";
+import { Center, Grid, GridItem } from "@chakra-ui/react";
+import Logo from "@/components/Logo";
+import Name from "@/components/Name";
+
+export default function Project() {
+  return (
+    <>
+      <Center  mb="80px" mt="20">
+        
+          <Grid>
+            <GridItem>
+              <Logo />
+            </GridItem>
+            <GridItem>
+              <Name />
+            </GridItem>
+            <GridItem>
+             
+            </GridItem>
+          </Grid>
+    
+      </Center>
+    </>
+  );
 }
