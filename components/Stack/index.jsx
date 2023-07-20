c7d7074c4914efeb587a9b9fc6c54ea0f2a66b53
ptahemdjehuty <pariso03henri@gmail.com>
2023-07-20 07:42:55 +0100

    ✏️ update tech stack component

--- components/Stack/index.jsx
+++ components/Stack/index.jsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Box, Center, Image, Flex } from "@chakra-ui/react";
+import { SimpleGrid, GridItem, Box, Center, Image, Flex } from "@chakra-ui/react";
 
 export default function Stack() {
   return (
@@ -6,77 +6,76 @@ export default function Stack() {
       <Center>
 
       <Flex>
-      <Grid templateColumns="repeat(9, 1fr)" gap={2} pt="10px" mx="10">
-          <GridItem w="100%"  h="100%">
-            <Box  >
+      <SimpleGrid columns={{ base: "4", md: "4", lg: "8", sm: "4" }} spacing={"10"} pt="10px" mx="10">
+          
+            <Box  boxSize={"50px"}>
               <Center>
                 <Image src="img/python.png"   alt="python"></Image>
               </Center>
             </Box>
-          </GridItem>
-          
-          <GridItem w="100%"  h="100%">
-            <Box  >
-              <Center>
-                <Image src="img/django.png"   alt="django"></Image>
-              </Center>
-            </Box>
-          </GridItem>
-
-          <GridItem w="100%"  h="100%">
-            <Box  >
+         
+       
+            <Box boxSize={"50px"}  >
               <Center>
                 <Image src="img/php.png"   alt="php"></Image>
               </Center>
             </Box>
-          </GridItem>
-
-          <GridItem w="100%"  h="100%">
-            <Box  >
-              <Center>
-                <Image src="img/postgresql.png"   alt="postgresql"></Image>
-              </Center>
-            </Box>
-          </GridItem>
-
-          <GridItem w="100%"  h="100%">
-            <Box  >
+        
+       
+            <Box  boxSize={"50px"} >
               <Center>
                 <Image src="img/javascript.png"   alt="javascript"></Image>
               </Center>
             </Box>
-          </GridItem>
+ 
 
-          <GridItem w="100%"  h="100%">
-            <Box  >
+    
+            <Box boxSize={"50px"} >
               <Center>
-                <Image src="img/react.png"   alt="react"></Image>
+                <Image src="img/kt.png"   alt="kotlin"></Image>
               </Center>
             </Box>
-          </GridItem>
-          <GridItem w="100%"  h="100%">
-            <Box  >
+
+
+        
+            <Box boxSize={"50px"}  >
               <Center>
-                <Image src="img/vue.png"   alt="vue"></Image>
+                <Image src="img/django.png"   alt="django"></Image>
               </Center>
             </Box>
-          </GridItem>
+       
 
-          <GridItem w="100%"  h="100%">
-            <Box  >
+            <Box boxSize={"50px"}  >
               <Center>
                 <Image src="img/docker.png"   alt="docker"></Image>
               </Center>
             </Box>
-          </GridItem>
-          <GridItem w="100%"  h="100%">
-            <Box  >
+       
+            <Box boxSize={"50px"}  >
               <Center>
-                <Image src="img/figma.png"   alt="docker"></Image>
+                <Image src="img/nuxt.png"   alt="nuxt"></Image>
               </Center>
             </Box>
-          </GridItem>
-        </Grid>  
+     
+            <Box boxSize={"50px"} >
+              <Center>
+                <Image src="img/react.png"   alt="react"></Image>
+              </Center>
+            </Box>
+         
+           {/*  <Box  >
+              <Center>
+                <Image src="img/vue.png"   alt="vue"></Image>
+              </Center>
+            </Box>
+     
+            <Box  >
+              <Center>
+                <Image src="img/kt.png"   alt="kotlin"></Image>
+              </Center>
+            </Box>
+         */}
+        </SimpleGrid>  
       </Flex>   
      
       </Center>
