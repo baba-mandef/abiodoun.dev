149738c96cbe22c732d9e55725b941cfd100a558
baba-mandef <contact@abiodoun.dev>
2024-01-29 00:00:28 +0100

    ✏️ update stack

--- components/Stack/index.jsx
+++ components/Stack/index.jsx
@@ -1,28 +1,35 @@
-import { SimpleGrid, GridItem, Box, Center, Image, Flex } from "@chakra-ui/react";
+import { SimpleGrid, Box, Center, Image, } from "@chakra-ui/react";
 
 export default function Stack() {
+  const styles = {
+
+    padding: '5px',
+    borderRadius: '10px',
+    boxShadow: '0px 4px 4px rgba(0, 10, 64, 0.51)',
+  }
   return (
     <>
+
       <Center>
 
     
-      <SimpleGrid columns={{ base: "4", md: "4", lg: "8", sm: "4" }} spacing={{ base: "10", md: "10", lg: "10", sm: "7" }} pt="10px">
+      <SimpleGrid columns={{ base: "7", md: "7", lg: "7", sm: "7" }} spacing={{ base: "10", md: "10", lg: "10", sm: "7" }} pt="10px">
           
-            <Box  boxSize={"50px"}>
+            <Box  boxSize={"50px"} style={styles}>
               <Center>
                 <Image src="img/python.png"   alt="python"></Image>
               </Center>
             </Box>
          
        
-            <Box boxSize={"50px"}  >
+            <Box boxSize={"50px"} style={styles}>
               <Center>
                 <Image src="img/php.png"   alt="php"></Image>
               </Center>
             </Box>
         
        
-            <Box  boxSize={"50px"} >
+            <Box  boxSize={"50px"} style={styles}>
               <Center>
                 <Image src="img/javascript.png"   alt="javascript"></Image>
               </Center>
@@ -30,7 +37,7 @@ export default function Stack() {
  
 
     
-            <Box boxSize={"50px"} >
+            <Box boxSize={"50px"} style={styles}>
               <Center>
                 <Image src="img/kt.png"   alt="kotlin"></Image>
               </Center>
@@ -38,31 +45,27 @@ export default function Stack() {
 
 
         
-            <Box boxSize={"50px"}  >
+            <Box boxSize={"50px"}  style={styles}>
               <Center>
                 <Image src="img/django.png"   alt="django"></Image>
               </Center>
             </Box>
        
 
-            <Box boxSize={"50px"}  >
-              <Center>
-                <Image src="img/docker.png"   alt="docker"></Image>
-              </Center>
-            </Box>
-       
-            <Box boxSize={"50px"}  >
-              <Center>
-                <Image src="img/nuxt.png"   alt="nuxt"></Image>
-              </Center>
-            </Box>
-     
-            <Box boxSize={"50px"} >
+            <Box boxSize={"50px"} style={styles}>
               <Center>
                 <Image src="img/react.png"   alt="react"></Image>
               </Center>
             </Box>
   
+
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/docker.png"   alt="docker"></Image>
+              </Center>
+            </Box>
+       
+     
         </SimpleGrid>  
 
      
