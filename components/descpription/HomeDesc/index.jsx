77cc214c5dee80fa3d2478b2ebe412b38b3f0051
baba-mandef <contact@abiodoun.dev>
2024-01-29 00:44:19 +0100

    ✏️ update

--- components/descpription/HomeDesc/index.jsx
+++ components/descpription/HomeDesc/index.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Box, Center, Heading } from "@chakra-ui/react";
 
 export default function HomeDesc() {
   return (
     <Center>
       <Box>
-        <Heading as={"h6"} size={{base:"sm", md:"md", lg:"md" }} color={"brandark.500"} fontWeight={"bold"}>
-           Innover pour servir {" "}
+        <Heading
+          as={"h6"}
+          size={{ base: "sm", md: "md", lg: "md" }}
+          color={"brandark.500"}
+          fontWeight={"bold"}
+        >
+          Innover pour servir{" "}
           <Box color="brand.500" as="span">
             l'Humanité
           </Box>
