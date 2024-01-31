c78171d9dde73f1932d4ed128d34e472b7614281
baba-mandef <contact@abiodoun.dev>
2024-01-31 01:14:58 +0100

    🔌 connect project_list component with backend

--- components/ProjectList/index.jsx
+++ components/ProjectList/index.jsx
@@ -4,8 +4,14 @@ import {
   Center,
   Image,
   Heading,
-  AbsoluteCenter,
+  useToast
+ 
 } from "@chakra-ui/react";
+
+import Link from "next/link";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ProjectList() {
   const styles = {
@@ -14,6 +20,34 @@ export default function ProjectList() {
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
+  const toast = useToast();
+
+  const baseUrl = "https://api.abiodoun.dev/api/v1/";
+
+  const [projects, setProjects] = useState([]);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get(baseUrl + "work");
+      console.log(response.data);
+      setProjects(response.data);
+    } catch (e) {
+      console.error("Error", e);
+      
+      toast({
+        title: "Erreur",
+        description:
+          "Une erreur s'est produite lors du chargement des données",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
 
   return (
     <>
@@ -22,122 +56,33 @@ export default function ProjectList() {
           columns={{ base: "4", sm: "3", md: "4", lg: "4" }}
           spacing={{ base: "5", sm: "2", md: "5", lg: "10" }}
         >
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
+          {projects.map((project) => (
+            <Box key={project.id}>
+              <Link
+                color={"brand.500"}
+                href={"/projects/[slug]"}
+                as={`/projects/${project.slug}`}
+              >
+                <Box boxSize={"120px"} style={styles} borderRadius={10}>
+                  <Center>
+                    <Image
+                      mt="45px"
+                      boxSize="40px"
+                      src={project.logo}
+                      alt="banner"
+                    ></Image>
+                  </Center>
+                </Box>
+                <Center>
+                  <Box maxW="120px">
+                    <Heading as="h6" size="x6">
+                      {project.name}
+                    </Heading>
+                  </Box>
+                </Center>
+              </Link>
             </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
-
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
-            </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
-
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
-            </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
-            </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
-            </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
-          <Box>
-            <Box boxSize={"120px"} style={styles}>
-              <Center>
-                <Image
-                  mt="40px"
-                  boxSize="50px"
-                  src="img/python.png"
-                  alt="python"
-                ></Image>
-              </Center>
-            </Box>
-            <Center>
-              <Box maxW="120px">
-                <Heading as="h6" size="x6">
-                  Investing bot{" "}
-                </Heading>
-              </Box>
-            </Center>
-          </Box>
+          ))}
         </SimpleGrid>
       </Center>
     </>
