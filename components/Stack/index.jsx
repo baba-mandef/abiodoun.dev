e961db18ad3ccf9e343b00c604406688e1d03d78
baba-mandef <contact@abiodoun.dev>
2025-05-06 07:24:16 +0100

    📝 update stack

--- components/Stack/index.jsx
+++ components/Stack/index.jsx
@@ -13,7 +13,7 @@ export default function Stack() {
       <Center>
 
     
-      <SimpleGrid columns={{ base: "7", md: "7", lg: "7", sm: "7" }} spacing={{ base: "10", md: "10", lg: "10", sm: "7" }} pt="10px">
+      <SimpleGrid columns={{ base: "8", md: "8", lg: "8", sm: "4",}} spacing={{ base: "10", md: "10", lg: "10", sm: "8" }} pt="10px">
           
             <Box  boxSize={"50px"} style={styles}>
               <Center>
@@ -39,7 +39,7 @@ export default function Stack() {
     
             <Box boxSize={"50px"} style={styles}>
               <Center>
-                <Image src="img/kt.png"   alt="kotlin"></Image>
+                <Image src="img/dart.png"   alt="dart"></Image>
               </Center>
             </Box>
 
@@ -51,19 +51,68 @@ export default function Stack() {
               </Center>
             </Box>
        
-
             <Box boxSize={"50px"} style={styles}>
               <Center>
-                <Image src="img/react.png"   alt="react"></Image>
+                <Image src="img/vue.png"   alt="vue"></Image>
               </Center>
             </Box>
-  
 
             <Box boxSize={"50px"}  style={styles}>
               <Center>
                 <Image src="img/docker.png"   alt="docker"></Image>
               </Center>
             </Box>
+
+            <Box boxSize={"50px"} style={styles}>
+              <Center>
+                <Image src="img/react.png"   alt="react"></Image>
+              </Center>
+            </Box>
+
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/flutter.png"   alt="flutter"></Image>
+              </Center>
+            </Box>
+  
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/electron.png"   alt="mysql"></Image>
+              </Center>
+            </Box>
+
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/mysql.png"   alt="mysql"></Image>
+              </Center>
+            </Box>
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/postgresql.png"   alt="postgresql"></Image>
+              </Center>
+            </Box>
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/gimp.png"   alt="gimp"></Image>
+              </Center>
+            </Box>
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/figma.png"   alt="figma"></Image>
+              </Center>
+            </Box>
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/ink.png"   alt="inkscape"></Image>
+              </Center>
+            </Box>
+            <Box boxSize={"50px"}  style={styles}>
+              <Center>
+                <Image src="img/kdenlive.png"   alt="kdenlive"></Image>
+              </Center>
+            </Box>
+
+            
        
      
         </SimpleGrid>  
